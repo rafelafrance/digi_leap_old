fdee1c5712d691333b57c58d8d6e713391c46c64
rafe <raphael.lafrance@gmail.com>
2021-05-10 14:23:03 -0400

    Move code for scoring OCR from notebooks to a library

---
+++ digi_leap/ocr.py
@@ -0,0 +1,140 @@
+"""OCR images."""
+
+import re
+from pathlib import Path
+
+import numpy as np
+import pytesseract
+from PIL import Image, ImageOps
+from scipy import ndimage
+from skimage import filters, morphology as morph
+
+from digi_leap import label_image as li
+from digi_leap.ocr_score import ocr_score
+
+# TODO: Constants like this should get calculated
+# For threshold_sauvola
+WINDOW_SIZE = 11
+K = 0.032
+
+# For remove_small_objects
+MIN_SIZE = 64
+
+# For ImageOps.scale
+MIN_DIM = 512
+FACTOR = 2.0
+
+
+def ocr_label(path: Path):
+    """Try to OCR the image."""
+    image = Image.open(path).convert('L')
+
+    # ################################################################
+    # Try the unmodified label
+
+    action = 'did nothing'
+
+    best = ocr_score(image)
+
+    if best.is_ok:
+        return best.update(path, action)
+
+    # ################################################################
+    # Try a bigger label
+
+    action = f'scaled by: {FACTOR}'
+
+    bigger = ImageOps.scale(image, FACTOR)
+    score = ocr_score(bigger)
+
+    if image.width < MIN_DIM or image.height < MIN_DIM:
+        image = bigger
+        best.log(action)
+
+    if score > best:
+        image = bigger
+        best = score
+        best.log(action)
+        if best.is_ok:
+            return best.update(path, action)
+
+    # ################################################################
+    # Try to orient the label
+
+    osd = pytesseract.image_to_osd(image)
+    angle = int(re.search(r'degrees:\s*(\d+)', osd).group(1))
+
+    action = f'rotated by: {angle}'
+
+    data = np.asarray(image).copy()
+
+    if angle != 0:
+        rotated = ndimage.rotate(data, int(angle), mode='nearest')
+        rotated = li.to_pil(rotated)
+        score = ocr_score(rotated)
+
+        if score > best:
+            image = rotated
+            best = score
+            best.log(action)
+            if best.is_ok:
+                return best.update(path, action)
+
+    # ################################################################
+    # Try converting the image to binary
+
+    action = f'sauvola threshold window size = {WINDOW_SIZE} K = {K}'
+
+    binary = np.asarray(image).copy()
+    threshold = filters.threshold_sauvola(binary, window_size=WINDOW_SIZE, k=K)
+    binary = binary < threshold
+
+    temp = li.to_pil(binary)
+    score = ocr_score(temp)
+
+    if score > best:
+        best = score
+        score.log(action)
+        if score.is_ok:
+            return score.update(path, action)
+
+    # ################################################################
+    # Try to remove small objects
+
+    action = f'removed small objects min_size = {MIN_SIZE}'
+
+    cleaned = morph.remove_small_objects(binary, min_size=MIN_SIZE)
+    score = ocr_score(li.to_pil(cleaned))
+
+    if score > best:
+        binary = cleaned
+        best = score
+        score.log(action)
+        if score.is_ok:
+            return score.update(path, action)
+
+    # ################################################################
+    # Try opening holes
+
+    action = f'binary opening'
+
+    cleaned = morph.binary_opening(binary)
+    score = ocr_score(li.to_pil(cleaned))
+
+    if score > best:
+        # binary = cleaned
+        best = score
+        score.log(action)
+        if score.is_ok:
+            return score.update(path, action)
+
+    # ################################################################
+    # Try to correct skew
+
+    # ################################################################
+    # Try to remove horizontal lines
+
+    # ################################################################
+    # Nothing worked
+
+    return best.update(path, 'fail')
---
+++ digi_leap/ocr_score.py
@@ -0,0 +1,106 @@
+"""Handle OCR scores."""
+import re
+import string
+
+from dataclasses import dataclass, field
+import textwrap
+
+import enchant
+import pytesseract
+
+from digi_leap import label_image as li
+from digi_leap.const import DATA_DIR
+
+OK = 90.0
+BAD = 20.0
+
+# How many words should be in a label
+MIN_WORDS = 20
+
+
+PUNCT = re.escape(string.punctuation)
+SPLIT = re.compile(rf'([\s{PUNCT}]+)')
+ALLOW = {'.)', '.]'}
+LANG = 'en_US'
+EXTRA_VOCAB = DATA_DIR / 'custom_vocab.txt'
+
+
+def spell_checker(lang, extra_vocab=''):
+    """Setup the spell checker."""
+    if extra_vocab:
+        vocab = enchant.DictWithPWL(lang, str(extra_vocab))
+    else:
+        vocab = enchant.Dict(lang)
+    return vocab
+
+
+VOCAB = spell_checker(LANG, EXTRA_VOCAB)
+
+
+@dataclass(order=True)
+class OCRScore:
+    """Handle OCR scores."""
+
+    found: int = 0
+    total: int = 0
+    file: str = ''
+    stem: str = ''
+    method: list[str] = field(default_factory=list)
+    text: str = ''
+
+    @property
+    def score(self):
+        """Score the results."""
+        return self.found, self.percent
+
+    @property
+    def is_ok(self):
+        """Is the score good enough?"""
+        return self.percent >= OK and self.found >= MIN_WORDS
+
+    @property
+    def percent(self):
+        """Calculate the percent of found words."""
+        per = self.found / self.total if self.total != 0 else 0.0
+        return round(per * 100.0, 2)
+
+    def __str__(self):
+        return textwrap.dedent(f"""
+        {self.score=}
+        {self.found=}
+        {self.percent=}
+        {self.total=}
+        {self.stem=}
+        {self.method=}
+        """)
+
+    def update(self, path, method):
+        """Update the score."""
+        self.file = str(path)
+        self.stem = path.stem
+        self.log(method)
+        return self
+
+    def log(self, action):
+        """Log the OCR action."""
+        if not self.method or action != self.method[-1]:
+            self.method.append(action)
+
+
+def ocr_score(image):
+    """Score OCR results using the spell checker as a proxy for quality."""
+    text = pytesseract.image_to_string(image, config=li.TESS_CONFIG)
+    text = re.sub(r'[\r\f]', '\n', text)
+    text = re.sub(r'(\n\s*){3,}', '\n\n', text)
+    text = text.strip()
+
+    words = [x for w in SPLIT.split(text) if (x := w.strip())]
+
+    found = sum(1 for w in words if VOCAB.check(w)
+                or len(w) == 1 or w in ALLOW)
+
+    return OCRScore(
+        total=len(words),
+        found=found,
+        text=text,
+    )
