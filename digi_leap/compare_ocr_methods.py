12e0a78a66c71a220d2f6cb048a02fb37f1218bf
rafelafrance <raphael.lafrance@gmail.com>
2022-06-13 17:34:17 -0400

    WIP adding scipt to compare OCR methods

--- digi_leap/compare_ocr_methods.py
+++ digi_leap/compare_ocr_methods.py
@@ -12,6 +12,7 @@ from pylib import log
 from pylib.ocr import engine_runner as er
 from pylib.ocr import label_transformer as lt
 
+from digi_leap.pylib.db import db
 from digi_leap.pylib.label_builder.line_align import line_align_py  # noqa
 
 
@@ -20,6 +21,7 @@ def main():
     args = parse_args()
 
     with sqlite3.connect(args.database) as cxn:
+        run_id = db.insert_run(cxn, args)
         aligner = line_align_py.LineAlign()
 
         sql = "select * from gold_standard where gold_set = ?"
@@ -28,68 +30,73 @@ def main():
         scores = get_scores(aligner, golden)
         output_scores(scores)
 
+        db.update_run_finished(cxn, run_id)
+
     log.finished()
 
 
-def get_scores(la, golden):
-    scores = {
-        "tesseract": score_tesseract(la, golden),
-        "tesseract_deskew": score_tesseract(la, golden, pipeline="deskew"),
-        "tesseract_binarize": score_tesseract(la, golden, pipeline="binarize"),
-        "tesseract_denoise": score_tesseract(la, golden, pipeline="denoise"),
-        #
-        "easyocr": score_easyocr(la, golden),
-        "easyocr_deskew": score_easyocr(la, golden, pipeline="deskew"),
-        "easyocr_binarize": score_easyocr(la, golden, pipeline="binarize"),
-        "easyocr_denoise": score_easyocr(la, golden, pipeline="denoise"),
-        #
-        "ensemble": score_ensemble(la, golden),
-        "label_builder": score_label_builder(la, golden),
+def get_scores(aligner, golden):
+    scores = []
+
+    for gold in golden:
+        scores.append(score_tess(aligner, gold))
+        scores.append(score_tess(aligner, gold, pipeline="deskew"))
+        scores.append(score_tess(aligner, gold, pipeline="binarize"))
+        scores.append(score_tess(aligner, gold, pipeline="denoise"))
+
+        scores.append(score_easy(aligner, gold))
+        scores.append(score_easy(aligner, gold, pipeline="deskew"))
+        scores.append(score_easy(aligner, gold, pipeline="binarize"))
+        scores.append(score_easy(aligner, gold, pipeline="denoise"))
+
+        scores.append(score_consensus(aligner, gold))
+        scores.append(score_label_builder(aligner, gold))
+
+    return scores
+
+
+def score_easy(aligner, gold, pipeline=""):
+    image = read_label(gold, pipeline)
+
+    text = er.EngineConfig.easy_ocr.readtext(
+        image, blocklist=er.EngineConfig.char_blacklist, detail=0
+    )
+    text = " ".join(text)
+
+    score = {
+        "engine": "easy",
+        "pipeline": pipeline,
+        "text": text,
+        "levenshtein": aligner.levenshtein(gold["gold_text"], text),
     }
-    return scores
+
+    return score
 
 
-def score_easyocr(aligner, golden, pipeline=""):
-    scores = {}
+def score_tess(aligner, gold, pipeline=""):
+    image = read_label(gold, pipeline)
+    text = pytesseract.image_to_data(image, config=er.EngineConfig.tess_config)
 
-    for gold in golden:
-        image = read_label(gold, pipeline)
+    score = {
+        "engine": "tesseract",
+        "pipeline": pipeline,
+        "text": text,
+        "levenshtein": aligner.levenshtein(gold["gold_text"], text),
+    }
 
-        text = er.EngineConfig.easy_ocr.readtext(
-            image, blocklist=er.EngineConfig.char_blacklist, detail=0
-        )
-        text = " ".join(text)
-
-        scores[gold["gold_id"]] = aligner.levenshtein(gold["gold_text"], text)
-
-    return scores
+    return score
 
 
-def score_tesseract(aligner, golden, pipeline=""):
-    scores = {}
-
-    for gold in golden:
-        image = read_label(gold, pipeline)
-        text = pytesseract.image_to_data(image, config=er.EngineConfig.tess_config)
-        scores[gold["gold_id"]] = aligner.levenshtein(gold["gold_text"], text)
-
-    return scores
+def score_consensus(aligner, gold):
+    text = ""
+    score = aligner.levenshtein(gold["gold_text"], text)
+    return score
 
 
-def score_ensemble(aligner, golden):
-    scores = {}
-    for gold in golden:
-        gold_id = gold["gold_id"]
-        scores[gold_id] = aligner.levenshtein()
-    return scores
-
-
-def score_label_builder(aligner, golden):
-    scores = {}
-    for gold in golden:
-        gold_id = gold["gold_id"]
-        scores[gold_id] = aligner.levenshtein()
-    return scores
+def score_label_builder(aligner, gold):
+    text = ""
+    score = aligner.levenshtein(gold["gold_text"], text)
+    return score
 
 
 def read_label(gold, pipeline=""):
