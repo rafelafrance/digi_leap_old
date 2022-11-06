4f72bbd4ff0e0cdd45d7e944ff5931b25bace905
rafelafrance <raphael.lafrance@gmail.com>
2022-11-06 16:23:11 -0500

    Digi-Leap client

--- static/client.js
+++ static/client.js
@@ -1,61 +1,116 @@
 'use strict';
 
-const CANVAS = document.querySelector('canvas');
-const CTX = CANVAS.getContext('2d');
-CTX.canvas.width = 1000;
-CTX.canvas.height = 1000;
+class Picture {
+    constructor(id) {
+        const canvas = document.getElementById(id);
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.full_size = new Image();
+        this.scale = {x: 1.0, y: 1.0};
+        this.before_boxes = null;
+        this.before_draw = null;
+    }
 
-let BEFORE_DRAW = null;
-let BEFORE_BOXES = null;
+    cloneCanvas() {
+        let newCanvas = document.createElement('canvas');
+        let ctx2 = newCanvas.getContext('2d');
 
-let SCALE = {x: 1.0, y: 1.0};
-let LABELS = [];
+        newCanvas.width = this.canvas.width;
+        newCanvas.height = this.canvas.height;
 
-const IMAGE = new Image();
+        ctx2.drawImage(this.canvas, 0, 0);
+        return newCanvas;
+    }
+}
 
-let START_POS = {x: 0, y: 0};
-let END_POS = {x: 0, y: 0};
-let FIXING = false;
+const SHEET = new Picture('sheet-canvas');
+const LABEL = new Picture('label-canvas');
+
+const DRAW = {
+    active: false,
+    start: {x: 0, y: 0},
+    end: {x: 0, y: 0},
+};
+
+let ALL_LABELS = [];
 
 
-async function displaySheet() {
+const scaleImage = (pic) => {
+    if (!pic.full_size) { return; }
+    const img_w = pic.full_size.width;
+    const img_h = pic.full_size.height;
+    const ctx_w = pic.ctx.canvas.width;
+    const ctx_h = pic.ctx.canvas.height;
+    let new_w = img_w;
+    let new_h = img_h;
+
+    pic.scale.x = 1.0;
+    pic.scale.y = 1.0;
+
+    if (img_w > ctx_w || img_h > ctx_h) {
+        new_h = ctx_h;
+        new_w = ctx_w;
+        if (img_h > img_w) {
+            new_w = new_h * img_w / img_h;
+        } else {
+            new_h = new_w * img_h / img_w;
+        }
+        pic.scale.x = new_w / img_w;
+        pic.scale.y = new_h / img_h;
+    }
+
+    pic.ctx.clearRect(0, 0, ctx_w, ctx_h);
+    pic.ctx.drawImage(pic.full_size, 0, 0, img_w, img_h, 0, 0, new_w, new_h);
+}
+
+
+const displaySheet = async () => {
+    const files = document.getElementById('files');
     if (!files.value) { return; }
 
     const url = await readFileAsDataURL(files.files[0]);
     await loadImage(url);
 
-    let width = IMAGE.width;
-    let height = IMAGE.height;
+    scaleImage(SHEET);
 
-    SCALE.x = 1.0;
-    SCALE.y = 1.0;
+    SHEET.before_boxes = SHEET.cloneCanvas();
 
-    if (IMAGE.width > CTX.canvas.width || IMAGE.height > CTX.canvas.height) {
-        if (IMAGE.height > IMAGE.width) {
-            let ratio = IMAGE.width / IMAGE.height;
-            height = CTX.canvas.height;
-            width = height * ratio;
-        } else {
-            let ratio = IMAGE.height / IMAGE.width;
-            width = CTX.canvas.width;
-            height = width * ratio;
-        }
-        SCALE.y = height / IMAGE.height;
-        SCALE.x = width / IMAGE.width;
-    }
-
-    CTX.clearRect(0, 0, CTX.canvas.width, CTX.canvas.height);
-    CTX.drawImage(IMAGE, 0, 0, IMAGE.width, IMAGE.height, 0, 0, width, height);
-    BEFORE_BOXES = cloneCanvas(CANVAS);
-
-    LABELS = [];
+    ALL_LABELS = [];
     setState();
 }
 
+const updateLabel = () => {
+    const index = document.getElementById('index');
+    const textarea = document.getElementById('text');
+    const enabled = ALL_LABELS.length != 0;
+    const label = ALL_LABELS[index.value - 1];
+    const info = document.getElementById('info');
+
+    textarea.value = enabled ? label.text : '';
+    info.value = `type: ${label.type}, x: ${label.left}, y: ${label.top}`;
+
+    const width = label.right - label.left;
+    const height = label.bottom - label.top;
+    const data = SHEET.full_size.ctx.getImageData(label.left, label.top, width, height);
+    LABEL.full_size.ctx.putImageData(data, 0, 0);
+    // LABEL.full_size = LABEL.ctx.drawImage(
+    //     SHEET.full_size,
+    //     label.left,
+    //     label.top,
+    //     width,
+    //     height,
+    //     0,
+    //     0,
+    //     width,
+    //     height,
+    // );
+    scaleImage(LABEL);
+}
+
 const loadImage = (url) => new Promise((resolve, reject) => {
-    IMAGE.addEventListener('load', () => resolve(IMAGE));
-    IMAGE.addEventListener('error', (err) => reject(err));
-    IMAGE.src = url;
+    SHEET.full_size.addEventListener('load', () => resolve(SHEET.full_size));
+    SHEET.full_size.addEventListener('error', (err) => reject(err));
+    SHEET.full_size.src = url;
 });
 
 function readFileAsDataURL(file) {
@@ -67,94 +122,80 @@ function readFileAsDataURL(file) {
     });
 }
 
-const cloneCanvas = (oldCanvas)  => {
-    let newCanvas = document.createElement('canvas');
-    let ctx2 = newCanvas.getContext('2d');
-
-    newCanvas.width = oldCanvas.width;
-    newCanvas.height = oldCanvas.height;
-
-    ctx2.drawImage(oldCanvas, 0, 0);
-
-    return newCanvas;
-}
-
 const canvasOffset = (evt) => {
     const {pageX, pageY} = evt.touches ? evt.touches[0] : evt;
-    const x = pageX - CANVAS.offsetLeft;
-    const y = pageY - CANVAS.offsetTop;
+    const x = pageX - SHEET.canvas.offsetLeft;
+    const y = pageY - SHEET.canvas.offsetTop;
     return {x, y};
 }
 
-const drawRectangle = async () => {
-    CTX.strokeStyle = "#d95f02";
-    CTX.lineWidth = 3;
-    CTX.strokeRect(
-        START_POS.x,
-        START_POS.y,
-        END_POS.x - START_POS.x,
-        END_POS.y - START_POS.y,
-    );
-}
 
 const drawBoxes = () => {
-    LABELS.forEach(lb => {
-        CTX.strokeStyle = lb.type.toLowerCase() == "typewritten" ? "#d95f02" : "#1b9e77";
-        CTX.lineWidth = 3;
-        CTX.strokeRect(
-            lb.left * SCALE.x,
-            lb.top * SCALE.y,
-            (lb.right - lb.left) * SCALE.x,
-            (lb.bottom - lb.top) * SCALE.y,
+    ALL_LABELS.forEach(lb => {
+        SHEET.ctx.strokeStyle = lb.type.toLowerCase() == 'typewritten' ? '#d95f02' : '#1b9e77';
+        SHEET.ctx.lineWidth = 3;
+        SHEET.ctx.strokeRect(
+            lb.left * SHEET.scale.x,
+            lb.top * SHEET.scale.y,
+            (lb.right - lb.left) * SHEET.scale.x,
+            (lb.bottom - lb.top) * SHEET.scale.y,
         );
     });
 }
 
 const mouseDownListener = (evt) => {
-    const fix = document.getElementById('fix').value;
-    if (fix == "Draw") {
-        START_POS = canvasOffset(evt);
-        FIXING = true;
-        BEFORE_DRAW = cloneCanvas(CANVAS);
-    } else if (["Typewritten", "Other", "Remove"].includes(fix)) {
+    const fixing = document.getElementById('fix').value;
+    if (fixing == 'Draw') {
+        DRAW.start = canvasOffset(evt);
+        DRAW.active = true;
+        SHEET.before_draw = SHEET.cloneCanvas();
+    } else if (['Typewritten', 'Other', 'Remove'].includes(fixing)) {
     } else {
         return false;
     }
 }
 
 const mouseMoveListener = (evt) => {
-    if(!FIXING) { return };
+    if(!DRAW.active) { return };
 
-    END_POS = canvasOffset(evt);
+    DRAW.end = canvasOffset(evt);
 
-    CTX.drawImage(BEFORE_DRAW, 0, 0);
+    SHEET.ctx.drawImage(SHEET.before_draw, 0, 0);
 
-    drawRectangle();
+    SHEET.ctx.strokeStyle = '#d95f02';
+    SHEET.ctx.lineWidth = 3;
+    SHEET.ctx.strokeRect(
+        DRAW.start.x,
+        DRAW.start.y,
+        DRAW.end.x - DRAW.start.x,
+        DRAW.end.y - DRAW.start.y,
+    );
 }
 
-const inLabel = (evt, lb) => {
+const insideLabel = (evt, lb) => {
     const pos = canvasOffset(evt);
-    pos.x /= SCALE.x;
-    pos.y /= SCALE.y;
-    return lb.left <= pos.x && lb.right >= pos.x && lb.top <= pos.y && lb.bottom >= pos.y;
+    pos.x /= SHEET.scale.x;
+    pos.y /= SHEET.scale.y;
+    return lb.left <= pos.x && lb.right >= pos.x
+        && lb.top <= pos.y && lb.bottom >= pos.y;
 }
 
 const mouseUpListener = (evt) => {
     const fix = document.getElementById('fix').value;
     if (fix == 'Draw') {
         // Build new label
-    } else if (["Typewritten", "Other"].includes(fix)) {
+    } else if (['Typewritten', 'Other'].includes(fix)) {
         const pos = canvasOffset(evt);
-        LABELS.forEach(lb => {
-            if (inLabel(evt, lb)) { lb.type = fix; }
+        ALL_LABELS.forEach(lb => {
+            if (insideLabel(evt, lb)) { lb.type = fix; }
         });
         drawBoxes();
-    } else if (fix == "Remove") {
-        CTX.drawImage(BEFORE_BOXES, 0, 0);
-        LABELS = LABELS.filter(lb => !inLabel(evt, lb));
+    } else if (fix == 'Remove') {
+        SHEET.ctx.drawImage(SHEET.before_boxes, 0, 0);
+        ALL_LABELS = ALL_LABELS.filter(lb => !insideLabel(evt, lb));
         drawBoxes();
     }
-    FIXING = false;
+    DRAW.active = false;
 }
 
 const findLabels = () => {
@@ -162,8 +203,8 @@ const findLabels = () => {
     const labelsFound = () => {
         if (req.readyState === XMLHttpRequest.DONE) {
             if (req.status === 200) {
-                LABELS = JSON.parse(req.responseText);
-                LABELS = JSON.parse(LABELS);  // WTF?!
+                ALL_LABELS = JSON.parse(req.responseText);
+                ALL_LABELS = JSON.parse(ALL_LABELS);  // WTF?!
                 drawBoxes();
                 setState();
             } else {
@@ -190,13 +231,12 @@ const ocrLabels = () => {
             if (req.status === 200) {
                 let labels = JSON.parse(req.responseText);
                 labels = JSON.parse(labels);  // WTF?!
-                LABELS = labels;
-                console.log(labels)
-                let text = "";
+                ALL_LABELS = labels;
+                let text = '';
                 labels.forEach(lb => {
                     text += lb.text;
                 });
-                document.querySelector('textarea').value = text;
+                document.getElementById('text').value = text;
                 setState();
             } else {
                 alert('OCR labels error');
@@ -204,10 +244,10 @@ const ocrLabels = () => {
         }
     }
     const data = new FormData();
-    data.append('labels', JSON.stringify(LABELS));
+    data.append('labels', JSON.stringify(ALL_LABELS));
     data.append('sheet', files.files[0]);
     data.append(
-        "filter",
+        'filter',
         document.querySelector('input[name="which"]:checked').value,
     );
 
@@ -218,59 +258,57 @@ const ocrLabels = () => {
     req.send(data);
 }
 
-const updateText = () => {
-    const index = document.getElementById("index");
-    const textarea = document.querySelector("textarea");
-    const active = LABELS.length != 0; // && !args.disabled;
-    const label = LABELS[index.value - 1];
-    textarea.value = active ? label.text : "";
-    info.value = !active ? "" : `${label.type} (${label.left}, ${label.top})`;
-}
-
 const setState = () => {
     const image_selected = !!files.value;
     const type = document.querySelector('input[name="type"]:checked').value;
-    const has_labels = LABELS.length != 0;
-    const has_text = LABELS.some(lb => !!lb.text);
+    const has_labels = ALL_LABELS.length != 0;
+    const has_text = ALL_LABELS.some(lb => !!lb.text);
 
     const toggleImageType = ({image_selected, type, has_labels}) => {
-        const sheet_ready = image_selected && type == "sheet";
+        const sheet_ready = image_selected && type == 'sheet';
         const labels_ready = sheet_ready && has_labels;
-        const can_ocr = image_selected && (type == "label" || (type == "sheet" && has_labels));
-        document.getElementById("find-labels").disabled = !image_selected || type != "sheet";
-        document.getElementById("ocr-labels").disabled = !can_ocr;
-        document.getElementById("conf").disabled = !sheet_ready;
+        const can_ocr = image_selected && (type == 'label' || (type == 'sheet' && has_labels));
+        document.getElementById('find-labels').disabled = !image_selected || type != 'sheet';
+        document.getElementById('ocr-labels').disabled = !can_ocr;
+        document.getElementById('conf').disabled = !sheet_ready;
         document.querySelectorAll('input[name="which"]').forEach(r => r.disabled = !sheet_ready);
-        document.getElementById("fix").disabled = !labels_ready;
-        document.getElementById("save-labels").disabled = !labels_ready;
+        document.getElementById('fix').disabled = !labels_ready;
+        document.getElementById('save-label').disabled = !labels_ready;
     }
 
     const toggleText = ({has_text}) => {
-        const index = document.getElementById("index");
-        index.disabled = !has_text;
-        index.min = has_text ? "1" : "0";
-        index.max = has_text ? `${LABELS.length}` : "0";
-        index.value = has_text ? "1" : "";
-
-        const textarea = document.querySelector("textarea");
-        textarea.disabled = !has_text;
-        textarea.value = has_text ? LABELS[0].text : "";
-
-        const info = document.getElementById("info");
-        info.value = has_text ? `${LABELS[0].type} (${LABELS[0].left}, ${LABELS[0].top})` : "";
-
-        document.getElementById("save-text").disabled = !has_text;
     }
 
-    if (!image_selected) {
-        LABELS = [];
-    }
+    if (!image_selected) { ALL_LABELS = []; }
+
     toggleImageType({image_selected, type, has_labels});
-    toggleText({has_text});
+
+    const index = document.getElementById('index');
+    index.disabled = !has_text;
+    index.min = has_text ? '1' : '0';
+    index.max = has_text ? `${ALL_LABELS.length}` : '0';
+    index.value = has_text ? '1' : '';
+
+    const textarea = document.getElementById('text');
+    textarea.disabled = !has_text;
+    textarea.value = has_text ? ALL_LABELS[0].text : '';
+
+    const info = document.getElementById('info');
+    info.value = '';
+    if (has_text) {
+        info.value = `${ALL_LABELS[0].type} `
+            + `(${ALL_LABELS[0].left}, ${ALL_LABELS[0].top})`;
+    }
+
+    document.getElementById('save-text').disabled = !has_text;
+
 }
 
+const showConf = () => {
+    document.getElementById('show-conf').value = conf.value;
+}
 
-const initialize = () => {
+(() => {
     displaySheet();
     setState();
     showConf();
@@ -285,26 +323,27 @@ const initialize = () => {
         .addEventListener('click', ocrLabels);
 
     document.getElementById('index')
-        .addEventListener('change', updateText);
+        .addEventListener('change', updateLabel);
 
-    document.querySelector('textarea')
+    document.getElementById('text')
         .addEventListener('change', () => {
-        const index = document.getElementById("index");
-        LABELS[index.value - 1].text = document.querySelector("textarea").value;
+        const index = document.getElementById('index');
+        ALL_LABELS[index.value - 1].text = document.getElementById('text').value;
     });
 
     document.getElementById('conf')
-        .addEventListener('change', () => {
-            document.getElementById("show-conf").value = conf.value;
-        });
+        .addEventListener('change', showConf);
 
     document.querySelectorAll('input[name="type"]')
         .forEach(r => r.addEventListener('change', setState));
 
+    SHEET.canvas.addEventListener('mousedown', mouseDownListener);
+    SHEET.canvas.addEventListener('mousemove', mouseMoveListener);
+    SHEET.canvas.addEventListener('mouseup', mouseUpListener);
 
-    CANVAS.addEventListener('mousedown', mouseDownListener);
-    CANVAS.addEventListener('mousemove', mouseMoveListener);
-    CANVAS.addEventListener('mouseup', mouseUpListener);
-}
-
-initialize();
+    let width = document.querySelector('.sheet').clientWidth;
+    SHEET.ctx.canvas.width = width;
+    SHEET.ctx.canvas.height = width;
+    LABEL.ctx.canvas.width = width;
+    LABEL.ctx.canvas.height = width;
+})();
