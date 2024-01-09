18f9753e9b7c91f5f38f3223d6f2de6a346eee87
rafelafrance <raphael.lafrance@gmail.com>
2024-01-09 17:12:51 -0500

    Squashed 'server/' content from commit 25bf377
    git-subtree-dir: server
    git-subtree-split: 25bf3771a3bfc9631dd523578598fe1859738bf7

---
+++ pylib/const.py
@@ -0,0 +1,5 @@
+# #########################################################################
+CLASSES = "Other Typewritten ".split()
+
+CLASS2INT = {c: i for i, c in enumerate(CLASSES)}
+CLASS2NAME = {v: k for k, v in CLASS2INT.items()}
